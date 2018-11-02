686bdbe1e8eba354ee2ae09705e573d78169fd36
RomanHudyma <roman.hudymaa@gmail.com>
2018-11-02 14:25:52 +0200

    [Home Page] Change Home page to func component

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,17 +1,14 @@
-import React, { PureComponent } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
 import { Header } from 'components/Header';
 
-
-class Home extends PureComponent {
-  render() {
-    return (
-      <div className="home-page">
-        <Header />
-        <Link to="/example">Check example page</Link>
-      </div>
-    );
-  }
-}
+const Home = () => {
+  return (
+    <div className="home-page">
+      <Header />
+      <Link to="/example">Check example page</Link>
+    </div>
+  );
+};
 
 export default Home;
