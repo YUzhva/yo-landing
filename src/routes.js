6cce7381ce389fade06ff6cfa1d92bdef5347920
Yevhen Uzhva <yevhen.uzhva@gmail.com>
2018-12-26 18:15:52 +0700

    [routes] Apply new Layout

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
+import { LayoutPublic } from 'containers';
+
 import {
   Home,
   Olenka,
@@ -10,10 +12,12 @@ import {
 const routes = (
   <Router>
     <Switch>
-      <Route exact path="/" component={Home} />
+      <LayoutPublic>
+        <Route exact path="/" component={Home} />
 
-      <Route exact path="/olenka" component={Olenka} />
-      <Route exact path="/yevhen" component={Yevhen} />
+        <Route exact path="/olenka" component={Olenka} />
+        <Route exact path="/yevhen" component={Yevhen} />
+      </LayoutPublic>
     </Switch>
   </Router>
 );
