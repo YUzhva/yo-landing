2138e51e5fa586f37546c45b6aed4e253a185fc6
Yevhen Uzhva <yevhen.uzhva@gmail.com>
2018-12-26 18:20:22 +0700

    [pages] Update Home

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,49 +1,84 @@
 import React from 'react';
-import { PageHeader } from 'components/PageHeader';
-import { Grid, Image, Header } from 'semantic-ui-react';
+import { Image } from 'react-bootstrap';
 
-import Persons from '../../media/images/Persons.svg';
-import Contribute from '../../media/images/Contribute.svg';
-import './Home.scss';
+import {
+  Button,
+  Heading,
+  Paragraph,
+  Spacing,
+} from 'components';
+import { Grid } from 'restyled';
+
+import Persons from './images/Persons.svg';
+import Contribute from './images/Contribute.svg';
 
 const Home = () => (
-  <div className="home-page">
-    <PageHeader />
-    <Grid container className="YO Home-page common-padding">
-      <Grid.Row>
-        <Grid.Column width="7" textAlign="left">
-          <Header className="font-size-xx-large circular-bold Home-page__title colored-yellow" size="huge">Bonjour</Header>
-          <Header className="font-size-x-large circular-bold Home-page__subtitile colored-black" size="large">
-            We are YO! mates —
-            <br />
-            Yevhen & Olenka.
-          </Header>
-          <p className="circular-medium font-size-large-em colored-black">We build awesome products with a power of user-friendly Design and fast Front-end development. We also enjoy to travel and try new beer tastes.</p>
-          <button
-            type="button"
-            className="Home-page__button Home-page__button--with-border Home-page__button--margin-top circular-bold colored-yellow font-size-medium-em"
-          >
-            start a project
-          </button>
-        </Grid.Column>
-        <Grid.Column width="1" />
-        <Grid.Column width="7" verticalAlign="middle">
-          <Image src={Persons} />
-        </Grid.Column>
-        <Grid.Column width="1" />
+  <div className="yo-home-page">
+    {/* SECTION 1 */}
+    <Grid>
+      <Grid.Row colEqualHeight>
+        <Grid.Col xs={12} md={5}>
+          {/* DEPENDING CONTENT */}
+          <Grid.Row>
+            <Grid.Col xs={12}>
+              <Heading color="yellow" typeface="bold">Bonjour</Heading>
+              <Spacing height={12} />
+              <Heading size={2} typeface="bold">
+                We are YO! mates — Yevhen & Olenka.
+              </Heading>
+              <Spacing height={24} />
+            </Grid.Col>
+            <Grid.Col xs={12}>
+              <Heading size={3} typeface="medium" justify>
+                We build awesome products with a power of user-friendly Design and
+                fast Front-end development.
+                We also enjoy to travel and try new beer tastes.
+              </Heading>
+            </Grid.Col>
+            <Grid.Col sm={12} mdHidden lgHidden>
+              <Image
+                src={Persons}
+                className="center-block"
+                alt="yevhen-and-olenka-as-lego"
+                responsive
+              />
+            </Grid.Col>
+            <Grid.Col xs={12}>
+              <Spacing height={48} />
+              <Button>
+                start a project
+              </Button>
+            </Grid.Col>
+          </Grid.Row>
+
+        </Grid.Col>
+
+        <Grid.Col md={6} mdOffset={1} xsHidden smHidden contentAlign="middle">
+          <Image
+            src={Persons}
+            className="center-block"
+            alt="yevhen-and-olenka-as-lego"
+            responsive
+          />
+        </Grid.Col>
       </Grid.Row>
     </Grid>
-    <Grid container padded="vertically" className="YO common-padding">
+
+    {/* SECTION 2 */}
+    <Grid className="yo-home-page__section">
       <Grid.Row>
-        <Grid.Column>
-          <Header className="font-size-x-large circular-bold Home-page__subtitile colored-black" size="large">
+        <Grid.Col xs={12}>
+          <Heading size={2} typeface="bold">
             Services, that we provide
-          </Header>
-        </Grid.Column>
+          </Heading>
+        </Grid.Col>
       </Grid.Row>
+
+      <Spacing height={48} />
+
       <Grid.Row>
-        <Grid.Column width="8">
-          <p className="circular-book colored-black">
+        <Grid.Col xs={12} sm={6} md={5}>
+          <Paragraph>
             UX design (defining personas, user scenarios, user stories, use cases)
             <br />
             Wireframing
@@ -53,10 +88,10 @@ const Home = () => (
             Mobile applications design
             <br />
             Interactive Prototypes
-          </p>
-        </Grid.Column>
-        <Grid.Column width="8">
-          <p className="circular-book colored-black">
+          </Paragraph>
+        </Grid.Col>
+        <Grid.Col xs={12} sm={6} md={5} mdOffset={1}>
+          <Paragraph>
             UX design (defining personas, user scenarios, user stories, use cases)
             <br />
             Wireframing
@@ -66,81 +101,110 @@ const Home = () => (
             Mobile applications design
             <br />
             Interactive Prototypes
-          </p>
-        </Grid.Column>
+          </Paragraph>
+        </Grid.Col>
       </Grid.Row>
     </Grid>
-    <Grid container padded="vertically" className="YO Home-page common-padding">
+
+    {/* SECTION 3 */}
+    <Grid className="yo-home-page__section">
       <Grid.Row>
-        <Grid.Column width="8" verticalAlign="middle">
-          <Header className="font-size-x-large circular-bold Home-page__subtitile colored-black" size="large">
+        <Grid.Col xs={12} sm={6}>
+          <Heading size={2} typeface="bold">
             Contributing to open-source
-          </Header>
-          <p className="circular-book colored-black">React leaflet markercluster.</p>
-          <button
-            type="button"
-            className="Home-page__button Home-page__button--margin-right circular-bold colored-yellow font-size-medium-em"
-          >
-            code
-          </button>
-          <button
-            type="button"
-            className="Home-page__button circular-bold colored-yellow font-size-medium-em"
-          >
-            demo
-          </button>
-        </Grid.Column>
-        <Grid.Column width="8">
-          <Image src={Contribute} />
-        </Grid.Column>
+          </Heading>
+
+          <Spacing height={48} />
+
+          <Paragraph>React leaflet markercluster.</Paragraph>
+          <Heading color="yellow" size={4} inline>
+            <span>code</span>
+            <Spacing width={12} />
+            <span>demo</span>
+          </Heading>
+        </Grid.Col>
+        <Grid.Col xs={12} sm={6}>
+          <Image src={Contribute} alt="lego-blocks" responsive />
+        </Grid.Col>
       </Grid.Row>
     </Grid>
-    <Grid container padded="vertically" className="YO Testimonials common-padding">
+
+    {/* SECTION 4 */}
+    <Grid className="yo-home-page__section">
       <Grid.Row>
-        <Grid.Column>
-          <Header className="font-size-x-large circular-bold Home-page__subtitile colored-black" size="large">
+        <Grid.Col xs={12}>
+          <Heading size={2} typeface="bold">
             Testimonials
-          </Header>
-        </Grid.Column>
+          </Heading>
+        </Grid.Col>
       </Grid.Row>
+
+      <Spacing height={48} />
+
       <Grid.Row>
-        <Grid.Column width="7">
-          <Header className="Testimonials__subtitle font-size-medium circular-bold colored-black" size="medium">for Olena</Header>
-          <div className="Testimonials__block">
-            <p className="circular-book colored-black">Olena has great designing skills and she gets work done quickly and on time. She is also clear, respectful and communicative. Olena also has good understanding and knowledge of the iOS design guidelines.</p>
-            <p className="colored-black circular-medium-italic">Upwork client, August 2018</p>
-          </div>
-          <div className="Testimonials__block">
-            <p className="circular-book colored-black">Within a few months of working with Olena on KindGeek she handled everything from reviewing the users needs, wireframing ideas complete with all necessary styling to implementing the full UI/UX expertise for our projects. Olena is proficient on all of the latest versions of the most cutting edge design software and is always eager to learn new skills. At Kindgeek she has worked on landing pages, mobile Apps, CRM systems, illustrations and business presentations for some of our clients and few inside projects. She is able to thoroughly focus on every project and is not easily distracted. Additionally, Olena has shown a leadership qualities and good communication skills with our clients.</p>
-            <p className="colored-black circular-medium-italic">Design manager @Kindgeek, May 2018</p>
-          </div>
-        </Grid.Column>
-        <Grid.Column width="1" />
-        <Grid.Column width="7">
-          <Header className="Testimonials__subtitle font-size-medium circular-bold colored-black" size="medium">for Yevhen</Header>
-          <div className="Testimonials__block">
-            <p className="circular-book colored-black">Olena has great designing skills and she gets work done quickly and on time. She is also clear, respectful and communicative. Olena also has good understanding and knowledge of the iOS design guidelines.</p>
-            <p className="colored-black circular-medium-italic">Upwork client, August 2018</p>
-          </div>
-          <div className="Testimonials__block">
-            <p className="circular-book colored-black">Within a few months of working with Olena on KindGeek she handled everything from reviewing the users needs, wireframing ideas complete with all necessary styling to implementing the full UI/UX expertise for our projects. Olena is proficient on all of the latest versions of the most cutting edge design software and is always eager to learn new skills. At Kindgeek she has worked on landing pages, mobile Apps, CRM systems, illustrations and business presentations for some of our clients and few inside projects. She is able to thoroughly focus on every project and is not easily distracted. Additionally, Olena has shown a leadership qualities and good communication skills with our clients.</p>
-            <p className="colored-black circular-medium-italic">Design manager @Kindgeek, May 2018</p>
-          </div>
-        </Grid.Column>
-        <Grid.Column width="1" />
-      </Grid.Row>
-    </Grid>
-    <Grid container padded="vertically" className="YO Contact common-padding">
-      <Grid.Row>
-        <Grid.Column width="7" verticalAlign="middle">
-          <Header className="font-size-xx-large circular-bold colored-black" size="huge">Let’s colaborate together on your next project!</Header>
-        </Grid.Column>
-        <Grid.Column width="1" />
-        <Grid.Column width="7" verticalAlign="middle">
-          <Header className="font-size-large circular-bold colored-black" size="medium">Contact us:</Header>
-          <p className="colored-black circular-book Contact__mail font-size-small">hello@yo-development.com</p>
-        </Grid.Column>
-        <Grid.Column width="1" />
+        <Grid.Col xs={12} sm={6} md={5}>
+          <Heading size={4} typeface="bold">
+            <span className="yo-home-page__title--decorated">for Olena</span>
+          </Heading>
+
+          <Spacing height={64} />
+
+          <Paragraph justify>
+            Olena has great designing skills and she gets work done quickly and
+            on time. She is also clear, respectful and communicative. Olena also
+            has good understanding and knowledge of the iOS design guidelines.
+            <Spacing height={24} />
+            <i>Upwork client, August 2018</i>
+          </Paragraph>
+          <Spacing height={48} />
+          <Paragraph justify>
+            Within a few months of working with Olena on KindGeek she handled
+            everything from reviewing the users needs, wireframing ideas
+            complete with all necessary styling to implementing the full UI/UX
+            expertise for our projects. Olena is proficient on all of the latest
+            versions of the most cutting edge design software and is always
+            eager to learn new skills. At Kindgeek she has worked on landing
+            pages, mobile Apps, CRM systems, illustrations and business
+            presentations for some of our clients and few inside projects.
+            She is able to thoroughly focus on every project and is not easily
+            distracted. Additionally, Olena has shown a leadership qualities
+            and good communication skills with our clients.
+            <Spacing height={24} />
+            <i>Design manager @Kindgeek, May 2018</i>
+          </Paragraph>
+        </Grid.Col>
+        <Grid.Col xs={12} sm={6} md={5} mdOffset={1}>
+          <Heading size={4} typeface="bold">
+            <span className="yo-home-page__title--decorated">for Yevhen</span>
+          </Heading>
+
+          <Spacing height={64} />
+
+          <Paragraph justify>
+            Olena has great designing skills and she gets work done quickly and
+            on time. She is also clear, respectful and communicative. Olena
+            also has good understanding and knowledge of the iOS design
+            guidelines.
+            <Spacing height={24} />
+            <i>Upwork client, August 2018</i>
+          </Paragraph>
+          <Spacing height={48} />
+          <Paragraph justify>
+            Within a few months of working with Olena on KindGeek she handled
+            everything from reviewing the users needs, wireframing ideas
+            complete with all necessary styling to implementing the full UI/UX
+            expertise for our projects. Olena is proficient on all of the latest
+            versions of the most cutting edge design software and is always
+            eager to learn new skills. At Kindgeek she has worked on landing
+            pages, mobile Apps, CRM systems, illustrations and business
+            presentations for some of our clients and few inside projects.
+            She is able to thoroughly focus on every project and is not easily
+            distracted. Additionally, Olena has shown a leadership qualities
+            and good communication skills with our clients.
+            <Spacing height={24} />
+            <i>Design manager @Kindgeek, May 2018</i>
+          </Paragraph>
+        </Grid.Col>
       </Grid.Row>
     </Grid>
   </div>
