708b91142409c7dc90d2e023ca81bfddbc4dfa95
Yevhen Uzhva <yevhen.uzhva@gmail.com>
2018-12-28 19:33:49 +0700

    [LayoutPublic] Implement loader support

--- src/containers/LayoutPublic/LayoutPublic.js
+++ src/containers/LayoutPublic/LayoutPublic.js
@@ -1,16 +1,61 @@
-import React from 'react';
+import React, { Component, Fragment } from 'react';
 import PropTypes from 'prop-types';
+
+import { Loader } from 'components';
 
 import { Header } from './Header';
 import { Footer } from './Footer';
 
-const LayoutPublic = ({ children }) => (
-  <div className="layout-public">
-    <Header />
-    {children}
-    <Footer />
-  </div>
-);
+class LayoutPublic extends Component {
+  state = {
+    isLandingLoadedPreviously: !!window.sessionStorage.getItem('isLandingLoadedPreviously'),
+  };
+
+  componentDidMount() {
+    this.isTimeoutFinished = false;
+    const { isLandingLoadedPreviously } = this.state;
+
+    if (!isLandingLoadedPreviously) {
+      // NOTE: because of avoiding loader animation during page refresh
+      // saving page loaded state in session storage
+      window.addEventListener('load', () => {
+        window.sessionStorage.setItem('isLandingLoadedPreviously', true);
+
+        if (this.isTimeoutFinished) {
+          this.setState({ isLandingLoadedPreviously: true });
+        }
+      });
+
+      // NOTE: if application was loaded faster
+      // let's has minimal animation time for Loader
+      setTimeout(() => {
+        this.isTimeoutFinished = true;
+        const isSessionStorageWasUpdated = !!window.sessionStorage.getItem('isLandingLoadedPreviously');
+
+        if (isSessionStorageWasUpdated) {
+          this.setState({ isLandingLoadedPreviously: true });
+        }
+      }, 1500);
+    }
+  }
+
+  render() {
+    return (
+      <div className="layout-public">
+        <Loader isInProgress={!this.state.isLandingLoadedPreviously} />
+        {
+          this.state.isLandingLoadedPreviously && (
+            <Fragment>
+              <Header />
+              {this.props.children}
+              <Footer />
+            </Fragment>
+          )
+        }
+      </div>
+    );
+  }
+}
 
 LayoutPublic.propTypes = {
   children: PropTypes.node.isRequired,
