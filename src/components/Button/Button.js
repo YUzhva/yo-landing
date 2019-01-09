f62675c044013f85d51b7d597353bb7f3bbd18bf
Yevhen Uzhva <yevhen.uzhva@gmail.com>
2019-01-09 14:13:30 +0700

    [Button] Add className prop support

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,14 +1,19 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const Button = ({ children }) => (
-  <button type="button" className="yo-btn">
+const Button = ({ children, className }) => (
+  <button type="button" className={`yo-btn${className && ` ${className}`}`}>
     {children}
   </button>
 );
 
+Button.defaultProps = {
+  className: '',
+};
+
 Button.propTypes = {
   children: PropTypes.node.isRequired,
+  className: PropTypes.string,
 };
 
 export default Button;
