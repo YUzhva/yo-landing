cfa430d8e79185504ef45546d7eb86b397aafe14
Yevhen Uzhva <yevhen.uzhva@gmail.com>
2019-01-09 14:14:08 +0700

    [Spacing] Add hiddenAt/visibleAt prop support

--- src/components/Spacing/Spacing.js
+++ src/components/Spacing/Spacing.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const Spacing = ({ height, width }) => (
-  <span className={`yo-spacing yo-spacing--height-${height} yo-spacing--width-${width}`} />
+const Spacing = ({
+  height, width, hiddenAt, visibleAt,
+}) => (
+  <span className={`yo-spacing yo-spacing--height-${height} yo-spacing--width-${width} yo-spacing--hidden-${hiddenAt} yo-spacing--visible-${visibleAt}`} />
 );
 
 Spacing.defaultProps = {
@@ -13,6 +15,8 @@ Spacing.defaultProps = {
 Spacing.propTypes = {
   height: PropTypes.oneOf([0, 12, 24, 48, 64]),
   width: PropTypes.oneOf([0, 12, 24, 48]),
+  hiddenAt: PropTypes.oneOf(['all', 'mobile', 'desktop']), // eslint-disable-line react/require-default-props
+  visibleAt: PropTypes.oneOf(['all', 'mobile', 'desktop']), // eslint-disable-line react/require-default-props
 };
 
 export default Spacing;
