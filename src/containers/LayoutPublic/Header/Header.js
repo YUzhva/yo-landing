a010c41ba196076ad899fcf959f7a0e3b74a2e4e
Yevhen Uzhva <yevhen.uzhva@gmail.com>
2019-01-11 14:34:50 +0700

    [Header] mobile navigation

--- src/containers/LayoutPublic/Header/Header.js
+++ src/containers/LayoutPublic/Header/Header.js
@@ -1,9 +1,11 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import { NavLink } from 'react-router-dom';
 import Image from 'react-bootstrap/lib/Image';
 
 import { Heading, Spacing } from 'components';
 import { Grid } from 'restyled';
+
+import ButtonMenu from './ButtonMenu';
 
 import YOLogo from './yo-logo.svg';
 
@@ -14,71 +16,82 @@ class Header extends Component {
     this.setState((prevState) => ({ isMobileMenuVisible: !prevState.isMobileMenuVisible }));
   }
 
+  closeMobileMenu = () => {
+    if (this.state.isMobileMenuVisible) this.setState({ isMobileMenuVisible: false });
+  }
+
   render() {
     return (
-      <div className="yo-header" data-aos="fade-down" data-aos-delay="250" data-aos-offset="0">
-        <Grid>
-          <Grid.Row colEqualHeight>
-            <Grid.Col xs={4}>
-              <NavLink to="/">
-                <Image src={YOLogo} className="yo-header__logo" alt="yo-logo" responsive />
-              </NavLink>
-            </Grid.Col>
+      <Fragment>
+        <div className="yo-header" data-aos="fade-down" data-aos-delay="250" data-aos-offset="0">
+          <Grid>
+            <Grid.Row colEqualHeight colMobileEqual="middle">
+              <Grid.Col xs={4}>
+                <NavLink to="/" onClick={this.closeMobileMenu}>
+                  <Image src={YOLogo} className="yo-header__logo" alt="yo-logo" responsive />
+                </NavLink>
+              </Grid.Col>
 
-            {/*
               <Grid.Col
-                xs={9}
+                xs={8}
                 className="text-right"
                 smHidden
                 mdHidden
                 lgHidden
               >
-                <button type="button" onClick={this.toggleMobileMenu}>
-                  =
-                </button>
+                <ButtonMenu
+                  isOpen={this.state.isMobileMenuVisible}
+                  onClick={this.toggleMobileMenu}
+                />
               </Grid.Col>
-            */}
 
-            <Grid.Col
-              sm={8}
-              className="text-right"
-              contentAlign="middle"
-              xsHidden
+              <Grid.Col
+                sm={8}
+                className="text-right"
+                contentAlign="middle"
+                xsHidden
+              >
+                <Heading size={4} typeface="bold" inline>
+                  <NavLink
+                    to="/olenka"
+                    className="yo-header__menu-item"
+                    activeClassName="yo-header__menu-item--active"
+                  >
+                    about Olenka
+                  </NavLink>
+                  <Spacing width={24} />
+                  <NavLink
+                    to="/yevhen"
+                    className="yo-header__menu-item"
+                    activeClassName="yo-header__menu-item--active"
+                  >
+                    about Yevhen
+                  </NavLink>
+                  {/*
+                  <Spacing width={24} />
+                  <NavLink
+                    to="#"
+                    className="yo-header__menu-item"
+                    activeClassName="yo-header__menu-item--active"
+                  >
+                    contact us
+                  </NavLink>
+                  */}
+                </Heading>
+              </Grid.Col>
+            </Grid.Row>
+          </Grid>
+        </div>
+
+        <div className={`yo-header__menu-mobile yo-header__menu-mobile--${this.state.isMobileMenuVisible ? 'visible' : 'invisible'}`}>
+          <Grid>
+            <Heading
+              size={4}
+              typeface="bold"
+              center
             >
-              <Heading size={4} typeface="bold" inline>
-                <NavLink
-                  to="/olenka"
-                  className="yo-header__menu-item"
-                  activeClassName="yo-header__menu-item--active"
-                >
-                  about Olenka
-                </NavLink>
-                <Spacing width={24} />
-                <NavLink
-                  to="/yevhen"
-                  className="yo-header__menu-item"
-                  activeClassName="yo-header__menu-item--active"
-                >
-                  about Yevhen
-                </NavLink>
-                {/*
-                <Spacing width={24} />
-                <NavLink
-                  to="#"
-                  className="yo-header__menu-item"
-                  activeClassName="yo-header__menu-item--active"
-                >
-                  contact us
-                </NavLink>
-                */}
-              </Heading>
-            </Grid.Col>
-          </Grid.Row>
-
-          {
-            this.state.isMobileMenuVisible && (
               <Grid.Row>
-                <Grid.Col xs={12}>
+                <Grid.Col xs={6} noPadding>
                   <NavLink
                     to="/olenka"
                     className="yo-header__menu-item"
@@ -87,8 +100,7 @@ class Header extends Component {
                     about Olenka
                   </NavLink>
                 </Grid.Col>
-
-                <Grid.Col width="16">
+                <Grid.Col xs={6} noPadding>
                   <NavLink
                     to="/yevhen"
                     className="yo-header__menu-item"
@@ -98,10 +110,10 @@ class Header extends Component {
                   </NavLink>
                 </Grid.Col>
               </Grid.Row>
-            )
-          }
-        </Grid>
-      </div>
+            </Heading>
+          </Grid>
+        </div>
+      </Fragment>
     );
   }
 }
