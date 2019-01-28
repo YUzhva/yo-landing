f759067048ad7e8dbd5b9631b3c1f30f20cf6b7e
Yevhen Uzhva <yevhen.uzhva@gmail.com>
2019-01-28 23:33:29 +0700

    [config] Setup manifest for assets and PWA

--- config/webpack/csr-webapp.prod.config.js
+++ config/webpack/csr-webapp.prod.config.js
@@ -1,6 +1,8 @@
 /* eslint-disable */
 var HtmlWebpackPlugin = require('html-webpack-plugin');
 var WorkboxWebpackPlugin = require('workbox-webpack-plugin');
+var WebpackPwaManifest = require('webpack-pwa-manifest');
+var ManifestPlugin = require('webpack-manifest-plugin');
 var merge = require('webpack-merge');
 
 var path = require('path');
@@ -18,11 +20,28 @@ module.exports = merge(csrWebappConfig, prodConfig, {
     new HtmlWebpackPlugin({
       template: path.join(paths.src, 'index.prod.template.html'),
       favicon: path.join(paths.src, 'media', 'favicon.png'),
+      minify: true,
     }),
     new WorkboxWebpackPlugin.GenerateSW({
       swDest: (process.env.SERVICE_WORKER_FILE_NAME || VARS.DEFAULT_SERVICE_WORKER_FILE_NAME) + '.js', // eslint-disable-line prefer-template
       clientsClaim: true,
       skipWaiting: true,
+    }),
+    new ManifestPlugin({
+      fileName: 'assets-manifest.json',
+    }),
+    new WebpackPwaManifest({
+      name: 'YO! mates',
+      short_name: 'YO! mates',
+      description: 'We build awesome products with the power of user-friendly Design and scalable Front-end architecture.',
+      background_color: '#ffce00',
+      icons: [
+        {
+          src: path.join(paths.src, 'media', 'favicon.png'),
+          sizes: [96, 128, 192, 256, 384, 512],
+          destination: 'dist/assets/icons-pwa',
+        },
+      ],
     }),
   ],
 });
--- config/webpack/ssr.prod.config.js
+++ config/webpack/ssr.prod.config.js
@@ -1,6 +1,8 @@
 /* eslint-disable */
 var HtmlWebpackPlugin = require('html-webpack-plugin');
 var WorkboxWebpackPlugin = require('workbox-webpack-plugin');
+var WebpackPwaManifest = require('webpack-pwa-manifest');
+var ManifestPlugin = require('webpack-manifest-plugin');
 var merge = require('webpack-merge');
 
 var path = require('path');
@@ -22,12 +24,29 @@ module.exports = [
         template: path.join(paths.src, 'index.prod.template.html'),
         filename: 'server.html',
         favicon: path.join(paths.src, 'media', 'favicon.png'),
+        minify: true,
       }),
       new WorkboxWebpackPlugin.GenerateSW({
         swDest: (process.env.SERVICE_WORKER_FILE_NAME || VARS.DEFAULT_SERVICE_WORKER_FILE_NAME) + '.js', // eslint-disable-line prefer-template
         clientsClaim: true,
         skipWaiting: true,
         directoryIndex: 'server.html',
+      }),
+      new ManifestPlugin({
+        fileName: 'assets-manifest.json',
+      }),
+      new WebpackPwaManifest({
+        name: 'YO! mates',
+        short_name: 'YO! mates',
+        description: 'We build awesome products with the power of user-friendly Design and scalable Front-end architecture.',
+        background_color: '#ffce00',
+        icons: [
+          {
+            src: path.join(paths.src, 'media', 'favicon.png'),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: 'dist/assets/icons-pwa',
+          },
+        ],
       }),
     ],
   }, prodConfig),
